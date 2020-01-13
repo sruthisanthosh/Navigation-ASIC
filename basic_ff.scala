df44f163a3ab4fbd3299e59c5a0a3b7f6ea40083
Sruthi Santhosh <35186002+sruthisanthosh@users.noreply.github.com>
2020-01-14 00:29:26 +0530

    Update basic_ff.scala

--- basic_ff.scala
+++ basic_ff.scala
@@ -1,3 +1,17 @@
 package problem
 
-import 
+import chisel3._
+
+class Block extends Module{
+val io= IO(new Bundle{
+val input=Input(0.U(4.W))
+val enable=Input((Bool())
+val output=Output(0.U(4.W))
+})
+  when(io.enable){
+    io.out := io.in
+  }
+  .otherwise{
+    io.out=0.U
+  }
+}

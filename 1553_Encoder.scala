362f33197283f68661dc3eb2ce0459dffbd493cd
Sruthi Santhosh <35186002+sruthisanthosh@users.noreply.github.com>
2020-01-18 00:40:16 +0530

    Update 1553_Encoder.scala

--- 1553_Encoder.scala
+++ 1553_Encoder.scala
@@ -4,10 +4,18 @@ import chisel3._
 
 class Encoder extends Module{
 val io = IO ( new Bundle{
-val StopClk   = Input(Bool())
-val Start   = Input(Bool())
-val Busy   = Input(Bool())
-
-
+val Start= Input(Bool())
+val Busy= Input(Bool())
+val data=Input(UInt(8.W))
+val out=Output(UInt(2.W))
 )}
+
+when(~Busy)
+{
+RegInit shifter= 0.U(16.W)
+Mux(data===1,io.out===10.b, io.out===01.b)
+Shifter := io.out
+Shifter <<2
 }
+}
+

a2b606b75f5f46010a00fd15bd378222cfc5fa5f
Sruthi Santhosh <35186002+sruthisanthosh@users.noreply.github.com>
2020-05-03 18:37:11 +0530

    Add files via upload

---
+++ main/status_word.scala
@@ -0,0 +1,34 @@
+package encdec
+
+import chisel3._
+import chisel3.util._
+
+class status extends Module{
+	val io=IO ( new Bundle{
+    val addr =Input(UInt(3.W))
+	val msg_error = Input(Bool())
+	val instr = Input(Bool())
+	val ser_req = Input(Bool())
+	val bcmd_rd = Input(Bool())
+	val busy = Input(Bool())
+	val subsys_flag = Input(Bool())
+	val dy_bus_accept = Input(Bool())
+	val ter_flag= Input(Bool())
+	val parity= Input(Bool())                               //add parity from outside
+	val mismatch = Output(Bool())
+	})
+}
+
+val status_word = RegInit(0.U(32.W))
+val checkparity= paritycheck := data.xorR                  //add data from outside
+
+when(checkparity === parity){
+	status_word := Cat(seq(address,0.U(5.W),msg_error,instr,ser_req,0.U(3.W),bcmd_rd,busy,subsys_flag,dy_bus_accept,ter_flag,parity))
+	mismatch := False.B
+}
+otherwise{
+	mismatch := True.B
+}
+
+
+
